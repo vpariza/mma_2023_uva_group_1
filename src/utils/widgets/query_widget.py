c7d851c4aaf11d9ee1299f81c188408b7dd1fd99
OlineRanum <oline.a.ranum@gmail.com>
2023-06-16 12:30:33 +0200

    Widgets - update query box design

--- src/utils/widgets/query_widget.py
+++ src/utils/widgets/query_widget.py
@@ -1,23 +1,43 @@
-from PyQt6.QtWidgets import QWidget, QLineEdit, QLabel, QPushButton
-
+from PyQt6.QtWidgets import QWidget, QLineEdit, QLabel, QPushButton, QVBoxLayout, QHBoxLayout
+from PyQt6.QtCore import QSize
 
 
 class QueryWidget(QWidget):
     def __init__(self):
         super().__init__()
-        
+        ## Combine widgets
+        self.vbox = QWidget(self)
+        vbox_layout = QVBoxLayout(self, spacing=2)
+        hbox =  QWidget(self)
+        hbox_layout = QHBoxLayout(self, spacing=5)
+
+        # Set box lable
         self.QueryLabel = QLabel(self)
-        self.QueryLabel.setText('Query:')
-        self.Query = QLineEdit(self)
-
-        self.Query.move(20, 40)
-        self.Query.resize(200, 32)
-        self.QueryLabel.move(20, 20)
-
-        pybutton = QPushButton('Search', self)
-        pybutton.clicked.connect(self.clickMethod)
-        pybutton.resize(200,32)
-        pybutton.move(20, 80)        
+        self.QueryLabel.setText('Free-text Query Search for House Descriptions:')
+        self.QueryLabel.setStyleSheet("border: 0px;")
+        vbox_layout.addWidget(self.QueryLabel)
+        
+        # Set query input box
+        self.QueryText = QLineEdit(self)
+        self.QueryText.setFixedSize(QSize(250, 25))
+        self.QueryText.setStyleSheet("border: 1px solid darkgray;")   
+        hbox_layout.addWidget(self.QueryText)
+        
+        # Set search button
+        self.QueryButton = QPushButton('Search Query', self)
+        self.QueryButton.clicked.connect(self.clickMethod)
+        self.QueryButton.setFixedSize(QSize(150, 25))  
+        self.QueryButton.setStyleSheet("border: 1px solid darkgray;")      
+        hbox_layout.addWidget(self.QueryButton)
+        
+        # Combine widgets
+        hbox.setLayout(hbox_layout)
+        hbox.setStyleSheet("border: 0px solid darkgray; background-color: #f5f5f5;")
+        vbox_layout.addWidget(hbox)
+        self.vbox.setLayout(vbox_layout)
+        # set widget style
+        self.vbox.resize(450, 100)
+        self.vbox.setStyleSheet("border: 2px solid darkgray; background-color: #f5f5f5;")
 
     def clickMethod(self):
-        print('Query: ' + self.Query.text())
+        print('Query: ' + self.QueryText.text())
