3211a8e42433e0e94cbb521e5ccebd6ea2bf4f72
Jona Ruthardt <jona@ruthardt.de>
2023-06-27 15:20:59 +0200

    bug fix

--- main_window.py
+++ main_window.py
@@ -67,7 +67,7 @@ class MainWindow(QMainWindow):
         # Define the Second Tab
         self._tab2_w = FeatureEngineeringWidget(data=self._data,
                                                 training_features=list(self._training_features.keys()), 
-                                                config=self._config, widgets={}, parent=self, img_paths = self._data['image_path'].tolist())
+                                                config=self._config, widgets={}, parent=self, img_paths = self._data['image_path'])
         self._tab2_w.updatedShowedData.connect(self.on_updated_showed_data_tab_2)
         self._tab2_w.txtQuerySubmitted.connect(self.on_query_submitted)
         self._tab2_w.modelToTrain.connect(self.on_train_model)
